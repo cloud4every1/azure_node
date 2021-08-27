201f9a58c3fcd0df3a000bef72e343dba72f5519
cloud4every1 <gerold.leeb@gmail.com>
2021-08-27 09:55:38 +0200

    emoved all network related code and changed to port 80

--- helloWorld.js
+++ helloWorld.js
@@ -2,12 +2,13 @@ var http = require('http');
 
 var os = require('os');
 
-var networkInterfaces = os.networkInterfaces();
+// var networkInterfaces = os.networkInterfaces();
 
-console.log(networkInterfaces[0]);
+// console.log(networkInterfaces[0]);
 
 
 http.createServer(function (req, res) {
   res.writeHead(200, { 'Content-Type': 'text/html' });
+  // res.end('Hello World!' + JSON.stringify(networkInterfaces));
   res.end('Hello World!' + JSON.stringify(networkInterfaces));
-}).listen(8080);
+}).listen(80);
