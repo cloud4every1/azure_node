8dc7b6e60113de4bed117ec60f0eceee396fa8f5
gerold leeb <gerold.leeb@gmail.com>
2021-08-27 09:12:59 +0200

    first commit

---
+++ helloWorld.js
@@ -0,0 +1,12 @@
+var http = require('http');
+
+var os = require('os');
+
+var networkInterfaces = os.networkInterfaces();
+
+console.log(networkInterfaces[0]);
+
+http.createServer(function (req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.end('Hello World!' + JSON.stringify(networkInterfaces));
+}).listen(8080);
